58e8049c5dba56d707ce2e82eb4ae1c545347b25
LorelizDev <loreliz.dev@gmail.com>
2024-11-25 18:07:24 +0100

    chore(db): configure sequelize connection and connect app.ts to database

--- app.ts
+++ app.ts
@@ -1,1 +1,35 @@
-//app
+import express from 'express';
+import db from './database/db';
+import { PORT } from './config';
+import cors from 'cors';
+
+export const app = express();
+const port = PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('This is the AssecoSync API');
+});
+
+app.use('/api');
+
+const startServer = async () => {
+  try {
+    await db.authenticate();
+    console.log('👍Connection has been established successfully.');
+
+    await db.sync();
+    console.log('✅ Database synced successfully.');
+  } catch (error) {
+    console.error('❌ Unable to connect to Database', error);
+    throw error;
+  }
+};
+
+export const server = app.listen(port, () => {
+  console.log(`🏃‍♂️ Server running on http://localhost:${port}`);
+});
+
+startServer();
--- database/db.ts
+++ database/db.ts
@@ -1,1 +1,16 @@
-//conexion a db
+import { Sequelize } from "sequelize";
+import { DB_HOST, DB_DEV_NAME, DB_PASSWORD, DB_USER, DB_TEST_NAME, NODE_ENV } from "../config";
+
+const isTest: boolean = NODE_ENV === 'test';
+const dbName: string = isTest ? DB_TEST_NAME! : DB_DEV_NAME!;
+
+const db = new Sequelize(dbName, DB_USER!, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "postgres",
+  logging: false, // Desactiva los registros de SQL
+  define: {
+    timestamps: false,
+  },
+});
+
+export default db;
