1c40997063d2eb1e8703821e2a5c4701f981b36e
LorelizDev <loreliz.dev@gmail.com>
2024-11-29 12:49:16 +0100

    feat(employees): implement getEmployeeById with authorization check for admin or matching user

--- controllers/employeeController.ts
+++ controllers/employeeController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
+import { Op } from 'sequelize';
 import Employee from '../models/employeeModel';
 import Department from '../models/departmentModel';
 import Role from '../models/roleModel';
-import { Op } from 'sequelize';
+import { CLIENT_ID } from '../config';
 
 export const getAllEmployees = async (req: Request, res: Response) => {
   try {
@@ -42,6 +43,61 @@ export const getAllEmployees = async (req: Request, res: Response) => {
       res.status(500).json({
         error:
           'An error occurred while retrieving the employees. Please try again later.',
+      });
+    }
+  }
+};
+
+export const getEmployeeById = async (req: Request, res: Response) => {
+  try {
+    const employeeId = req.params.id;
+    if (!employeeId) {
+      res.status(400).json({ error: 'Employee ID is required' });
+    }
+
+    const tokenContent = (req as any).kauth?.grant?.access_token.content;
+    const emailAuthenticated = tokenContent.email;
+    const isAdmin =
+      tokenContent.resource_access?.[CLIENT_ID!]?.roles.includes('admin');
+
+    const employee = await Employee.findByPk(employeeId, {
+      include: [
+        {
+          model: Department,
+          as: 'Department',
+          attributes: ['name'],
+        },
+        {
+          model: Role,
+          as: 'Role',
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    if (employee?.email === emailAuthenticated || isAdmin) {
+      res.status(200).json({
+        id: employee?.id,
+        email: employee?.email,
+        firstName: employee?.firstName,
+        lastName: employee?.lastName,
+        jobTitle: employee?.jobTitle,
+        weeklyHours: employee?.weeklyHours,
+        avatar: employee?.avatar,
+        department: employee?.Department?.name,
+        role: employee?.Role?.name,
+      });
+    } else {
+      res.status(404).json({
+        error: 'Employee not found or not authorized',
+      });
+    }
+  } catch (error) {
+    if (error) {
+      console.error('Error retrieving employee:', error);
+      res.status(500).json({
+        error:
+          'An error occurred while retrieving the employee. Please try again later.',
       });
     }
   }
--- routes/employeeRoutes.ts
+++ routes/employeeRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { getAllEmployees } from '../controllers/employeeController';
 import { keycloak } from '../middlewares/keycloak';
+import { getEmployeeById } from '../controllers/employeeController';
 
 const employeeRoutes = Router();
 
 employeeRoutes.get('/', keycloak.protect('admin'), getAllEmployees);
+employeeRoutes.get('/:id', keycloak.protect(), getEmployeeById);
 
 export default employeeRoutes;
